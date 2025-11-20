ccfd67ec3e4397368488487b31512df7dbfc45fc
NurbekT7 <sh7nurbek@gmail.com>
2025-11-20 12:50:41 +0600

    first

---
+++ src/com/devcolibri/Main.java
@@ -0,0 +1,21 @@
+package com.devcolibri;
+
+public class Main {
+    static byte b = 1;
+    static short sh = 10;
+    static int i = 345;
+    static long l = 123456789;
+
+    static float f = 4.399999f;
+    static double d = 4.5;
+
+    static boolean bool = true;
+
+    static char ch = 234;
+
+    static String lang = "English";
+
+    public static void main(String[] args){
+        System.out.println(lang + " " + ch);
+    }
+}
