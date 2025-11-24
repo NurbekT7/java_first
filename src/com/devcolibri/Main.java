634b02c59f74734daf9aabd1ba5e972945eaa9d7
Nurbek T <74821769+NurbekT7@users.noreply.github.com>
2025-11-24 14:19:56 +0600

    wtf

--- src/com/devcolibri/Main.java
+++ src/com/devcolibri/Main.java
@@ -1,21 +1,71 @@
-package com.devcolibri;
 
-public class Main {
-    static byte b = 1;
-    static short sh = 10;
-    static int i = 345;
-    static long l = 123456789;
+// public class Main {
+//     static byte b = 1;
+//     static short sh = 10;
+//     static int i = 345;
+//     static long l = 123456789;
 
-    static float f = 4.399999f;
-    static double d = 4.5;
+//     static float f = 4.399999f;
+//     static double d = 4.5;
 
-    static boolean bool = true;
+//     static boolean bool = true;
 
-    static char ch = 234;
+//     static char ch = 234;
 
-    static String lang = "English";
+//     static String lang = "English";
 
-    public static void main(String[] args){
-        System.out.println(lang + " " + ch);
-    }
-}
+//     public static void main(String[] args){
+//         System.out.println(lang + " " + ch);
+//     }
+// }
+
+// public class MyClass {
+//   static char as = 100;
+//   public static void main(String args[]) {
+//     // float a = 10 / 3;
+//     // double s = (int)a;
+//     int s = (int) as;
+//     System.out.println(as);
+//   }
+// }
+
+// import java.util.Scanner;
+
+// public class MyClass {
+//   public static void main(String args[]){
+//     int a = -4;
+  
+//     System.out.println(.1 + .2);
+//   }
+// }
+
+// import java.util.Scanner;
+
+// public class MyClass {
+//   static boolean bf = true;
+//   public static void main(String args[]){
+//     if (bf) {
+//       System.out.println("its True");
+//     }
+//     else{
+//       System.out.println("Its not True");
+//     }
+//   }
+// }
+
+// import java.util.Scanner;
+
+// public class MyClass {
+//   public static void main(String args[]) {
+//     Scanner scanner = new Scanner(System.in);
+//     String line = scanner.nextLine();
+//     int a = Integer.parseInt(line);
+    
+//     if (a != 2 && a % 2 ==0) {
+//       System.out.println("YES");
+//     }
+//     else{
+//       System.out.println("NO");
+//     }
+//   }
+// }
