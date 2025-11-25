9006cca7cac78f207cf040c754239f7b26dfd836
Nurbek T <74821769+NurbekT7@users.noreply.github.com>
2025-11-25 15:41:46 +0600

    function with return

---
+++ src/com/devcolibri/function.java
@@ -0,0 +1,10 @@
+public class MyClass {
+  public static void main(String args[]) {
+    System.out.println(mySum(1, 5));
+  }
+  
+  static int mySum(int a, int b){
+      int sum = a + b;
+      return sum + 5;
+  }
+}
