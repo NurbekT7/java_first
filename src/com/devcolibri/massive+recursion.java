f0183155b6f0c6e73f29dcc7d1e975376b5210fd
Nurbek T <74821769+NurbekT7@users.noreply.github.com>
2025-11-26 10:04:21 +0600

    massive+recursion

---
+++ src/com/devcolibri/massive+recursion.java
@@ -0,0 +1,26 @@
+public class Main{
+    
+    static String[] nums = {"Hello", "World", "MF"};
+
+    public static void main(String[] args){
+
+        for (String n: nums){
+            System.out.print(n);
+        }
+            }
+}
+
+
+// public class Main{
+//     public static void main(String[] args){
+//         System.out.println(summing(1, -1));
+//     }
+
+//     static int summing(int a, int b) {
+//         int sum = a + b;
+//         if (sum == 0) {
+//             sum = summing(1, 1);
+//         }
+//         return sum;
+//     }
+// }
